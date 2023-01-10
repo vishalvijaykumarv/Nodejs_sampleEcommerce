32c0963abe28d777fe22419b2ba227159e2e290c
Vishal vijayakumar <56822639+vishalvijaykumarv@users.noreply.github.com>
2023-01-10 23:51:01 +0530

    updating with user login ( not worked yet )

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
-
+const userHelpers=require('../helpers/user-helper')
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -9,5 +9,18 @@ router.get('/', function(req, res, next) {
     res.render('user/view-products',{products,admin:false})
   })
 });
+
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((responce)=>{
+    console.log(responce)
+  })
+})
 
 module.exports = router;
